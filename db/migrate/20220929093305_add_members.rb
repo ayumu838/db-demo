2c38e36bddd3475adfc8fc60b7fe15762645fcb4
ayumu838 <ayumu.mochizuki@livesense.co.jp>
2022-09-29 19:09:32 +0900

    fix migration

--- db/migrate/20220929093305_add_members.rb
+++ db/migrate/20220929093305_add_members.rb
@@ -1,6 +1,6 @@
 class AddMembers < ActiveRecord::Migration[7.0]
   def change
-    create_table :shops do |t|
+    create_table :members do |t|
       t.string :name, null: false
       t.string :mail, null: false
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_29_093312) do
+  create_table "companies", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "entries", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.bigint "work_id", null: false
+    t.bigint "member_id"
+    t.datetime "entry_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "fk_rails_963b1503cb"
+    t.index ["work_id"], name: "index_entries_on_work_id"
+  end
+
+  create_table "members", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "mail", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shops", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.bigint "company_id", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "fk_rails_ace4062881"
+  end
+
+  create_table "works", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.bigint "shop_id", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "salary", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_works_on_shop_id"
+  end
+
+  add_foreign_key "entries", "members"
+  add_foreign_key "entries", "works"
+  add_foreign_key "shops", "companies"
+  add_foreign_key "works", "shops"
 end
