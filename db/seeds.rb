7ec4f5905d6422e39415993ccf9a904cc42f89c2
ayumu838 <ayumu.mochizuki@livesense.co.jp>
2022-10-03 18:19:46 +0900

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+RECORD = 100_000
+
+Company.transaction do
+  RECORD.times do
+    Company.create(
+      name: Faker::Company.name,
+      address: Faker::Address.state + Faker::Address.city + Faker::Address.street_name + Faker::Address.state_abbr
+    )
+  end
+end
+
+Faker::Config.locale = :en
+
+Shop.transaction do
+  RECORD.times do
+    Shop.create(
+      company_id: Random.rand(RECORD),
+      name: Faker::Company.name,
+      description: 'dummy' * Random.rand(10)
+    )
+  end
+end
+
+Work.transaction do
+  (RECORD * 4).times do
+    Work.create(
+      shop_id: Random.rand(RECORD),
+      name: Faker::Company.name,
+      description: 'dummy' * Random.rand(10),
+      salary: 1000 + Random.rand(1000)
+    )
+  end
+end
+
+Faker::Config.locale = :ja
+
+Member.transaction do
+  (RECORD * 4).times do
+    Member.create(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+    )
+  end
+end
+
+Entry.transaction do
+  (RECORD * 5).times do
+    Entry.create(
+      work_id: Random.rand(RECORD * 4),
+      member_id: Random.rand(RECORD *  4)
+    )
+  end
+end
